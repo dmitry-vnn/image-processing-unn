f787dbce20ce3c8827c8d4c68f7d8a9d8a9e6b3e
Dmitry <xtopz1337@gmail.com>
2023-09-25 23:49:02 +0300

    Adapted to AbstractImagesComparator

--- src/main/kotlin/dmitry/metric/ImageMSEMetric.kt
+++ src/main/kotlin/dmitry/metric/ImageMSEMetric.kt
@@ -1,48 +1,26 @@
 package dmitry.metric
 
-import dmitry.converter.MonochromeGrayConverter
-import java.awt.Color
 import java.awt.image.BufferedImage
 import kotlin.math.pow
 
 
-class ImageMSEMetric(private val basicImage: BufferedImage, private val comparableImage: BufferedImage) : ImageMetric {
+class ImageMSEMetric(basicImage: BufferedImage, comparableImage: BufferedImage): AbstractImagesComparator(basicImage, comparableImage) {
 
-    init {
-        assertSameImagesSize()
-    }
-
-    private fun assertSameImagesSize() {
-        if (basicImage.height != comparableImage.height || basicImage.width != comparableImage.width)
-            throw IllegalArgumentException("images must be same size")
-    }
-
-    private fun convertImagesToGrayMonochrome() {
-        MonochromeGrayConverter(basicImage).convert()
-        MonochromeGrayConverter(comparableImage).convert()
-    }
-
-    override fun calculate(): Double {
-
-        convertImagesToGrayMonochrome();
-
+    override fun compareGrayMonochromeImages(): Double {
         var result = 0.0
 
-        for (y in 0..<basicImage.height) {
-            for (x in 0..<basicImage.width) {
-                result += ((basicImage.takeColor(x, y) - comparableImage.takeColor(x, y)).toDouble()).pow(2.0)
+        for (y in 0..<basicMonochromeImage.height) {
+            for (x in 0..<basicMonochromeImage.width) {
+                result += ((basicMonochromeImage.takeColor(x, y) - comparableMonochromeImage.takeColor(x, y)).toDouble()).pow(2.0)
             }
         }
 
-        result /= basicImage.height * basicImage.width
+        result /= basicMonochromeImage.height * basicMonochromeImage.width
 
         return result
     }
 
 
-    private fun BufferedImage.takeColor(x: Int, y: Int): Int {
-        return Color(getRGB(x, y)).red
-    }
 
 }
 
