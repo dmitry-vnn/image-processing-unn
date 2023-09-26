57ff82fe28aeddfccd5ce4aa411503d3a346341a
Dmitry <xtopz1337@gmail.com>
2023-09-26 15:55:52 +0300

    edit

--- src/test/kotlin/dmitry/metric/ImageMSEMetricTest.kt
+++ src/test/kotlin/dmitry/metric/ImageMSEMetricTest.kt
@@ -3,23 +3,15 @@ package dmitry.metric
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import java.awt.image.BufferedImage
 
 class ImageMSEMetricTest {
 
-    @Test
-    fun test1() {
-        assertTrue(true)
-    }
+    lateinit var baseImage: BufferedImage
+    lateinit var comparableImage: BufferedImage
 
-    @Test
-    fun test2() {
-    }
-
-    companion object {
-        @JvmStatic
-        @BeforeAll
-        fun loadImage(): Unit {
-            println("before")
-        }
+    @BeforeAll
+    fun load() {
+        baseImage = getC
     }
 }
