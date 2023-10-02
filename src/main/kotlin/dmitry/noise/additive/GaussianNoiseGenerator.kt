abd48daf3e47287a78e6ff25d9f1778332e191a2
Dmitry <xtopz1337@gmail.com>
2023-10-02 19:52:32 +0300

    add GaussianNoiseGenerator

---
+++ src/main/kotlin/dmitry/noise/additive/GaussianNoiseGenerator.kt
@@ -0,0 +1,32 @@
+package dmitry.noise.additive
+
+import dmitry.noise.AdditiveNoiseGenerator
+import java.awt.image.BufferedImage
+import kotlin.math.*
+
+class GaussianNoiseGenerator(
+    m: Double,
+    sigma: Double,
+
+    noisePercentage: Double,
+    image: BufferedImage,
+
+): AdditiveNoiseGenerator(image, noisePercentage) {
+
+    init {
+        normalizedProbabilityDistribution = DoubleArray(255)
+
+        var sum = 0.0
+
+        for (i in normalizedProbabilityDistribution.indices) {
+            normalizedProbabilityDistribution[i] = (1 / (sqrt(2 * PI) * sigma))  * E.pow( (i - m).pow(2) / -2 * sigma.pow(2) )
+            sum += normalizedProbabilityDistribution[i]
+        }
+
+        for (i in normalizedProbabilityDistribution.indices) {
+            normalizedProbabilityDistribution[i] /= sum
+        }
+    }
+}
+
+
