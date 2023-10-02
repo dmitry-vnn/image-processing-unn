4ae45b5ad9f7e62ca6152ede1ab343edcfe0d461
Dmitry <xtopz1337@gmail.com>
2023-10-02 23:38:57 +0300

    add ImageRegion integration

--- src/main/kotlin/dmitry/converter/PointFilter.kt
+++ src/main/kotlin/dmitry/converter/PointFilter.kt
@@ -1,15 +1,19 @@
 package dmitry.converter
 
+import dmitry.ImageRegion
 import java.awt.Color
 import java.awt.image.BufferedImage
 
-abstract class PointFilter(protected val image: BufferedImage): ImageConverter {
+abstract class PointFilter(
+    protected val image: BufferedImage,
+    private val region: ImageRegion = ImageRegion(image)
+): ImageConverter {
 
     protected abstract fun getColorFrom(color: Color): Color
 
     override fun convert(): BufferedImage {
-        for (y in 0..<image.height) {
-            for (x in 0..<image.width) {
+        for (y in region.lineHeight) {
+            for (x in region.lineWidth) {
                 image.setRGB(x, y, getColorFrom(Color(image.getRGB(x, y))).rgb)
             }
         }
