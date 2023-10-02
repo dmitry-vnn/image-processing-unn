dfa04a0c7eeaebb7ddfad8168b116bce5f76705a
Dmitry <xtopz1337@gmail.com>
2023-10-02 23:47:14 +0300

    few changes

--- src/main/kotlin/dmitry/example/GaussianNoiseExample.kt
+++ src/main/kotlin/dmitry/example/GaussianNoiseExample.kt
@@ -1,19 +1,19 @@
 package dmitry.example
 
+import dmitry.example.GaussianNoiseExample.inImagePath
+import dmitry.example.GaussianNoiseExample.outImagePath
 import dmitry.noise.additive.GaussianNoiseGenerator
 import dmitry.store.ImageLoader
 import dmitry.store.ImageSaver
-import java.awt.image.BufferedImage
-import javax.imageio.ImageIO
 
-class GaussianNoiseExample
-
-val inImagePath = ExamplePathEvaluator.defaultInputPath<GaussianNoiseExample>()
-val outImagePath = ExamplePathEvaluator.defaultOutputPath<GaussianNoiseExample>()
+private object GaussianNoiseExample {
+    val inImagePath = PathToExampleEvaluator.defaultInputPath<GaussianNoiseExample>()
+    val outImagePath = PathToExampleEvaluator.defaultOutputPath<GaussianNoiseExample>()
+}
 
 fun main() {
 
-    val generatedImage = GaussianNoiseGenerator(10.0, 20.0, .5, ImageLoader(inImagePath).load()).generate()
+    val generatedImage = GaussianNoiseGenerator(10.0, 20.0, .7, ImageLoader(inImagePath).load()).generate()
     ImageSaver(generatedImage, outImagePath).save()
 
 }
