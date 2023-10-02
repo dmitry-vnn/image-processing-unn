e44763eb8b93a804c0f726b0b6be364f0f9fa43c
Dmitry <xtopz1337@gmail.com>
2023-10-02 23:40:05 +0300

    add ImageRegion support

--- src/main/kotlin/dmitry/metric/AbstractImagesComparator.kt
+++ src/main/kotlin/dmitry/metric/AbstractImagesComparator.kt
@@ -1,25 +1,33 @@
 package dmitry.metric
 
+import dmitry.ImageRegion
 import dmitry.converter.MonochromeGrayConverter
 import java.awt.Color
 import java.awt.image.BufferedImage
 
-abstract class AbstractImagesComparator(protected val basicMonochromeImage: BufferedImage, protected val comparableMonochromeImage: BufferedImage): ImageMetric  {
+abstract class AbstractImagesComparator(
+    protected val basicMonochromeImage: BufferedImage,
+    protected val comparableMonochromeImage: BufferedImage,
+    protected val imageRegion: ImageRegion = ImageRegion(basicMonochromeImage)
+): ImageMetric  {
 
     init {
         assertSameImagesSize()
     }
 
     private fun assertSameImagesSize() {
-        if (basicMonochromeImage.height != comparableMonochromeImage.height || basicMonochromeImage.width != comparableMonochromeImage.width)
-            throw IllegalArgumentException("images must be same size")
+        if (!(imageRegion.regionStart.x >= 0 && imageRegion.regionStart.y >= 0  &&
+            imageRegion.regionEnd.x < basicMonochromeImage.width && imageRegion.regionEnd.x < comparableMonochromeImage.width &&
+            imageRegion.regionEnd.y < basicMonochromeImage.height && imageRegion.regionEnd.y < comparableMonochromeImage.height
+            ))
+            throw IllegalArgumentException("imageRegion must be include in both basic image and comparable image")
     }
 
     override fun calculate(): Double {
 
         fun convertImagesToGrayMonochrome() {
-            MonochromeGrayConverter(basicMonochromeImage).convert()
-            MonochromeGrayConverter(comparableMonochromeImage).convert()
+            MonochromeGrayConverter(basicMonochromeImage, imageRegion).convert()
+            MonochromeGrayConverter(comparableMonochromeImage, imageRegion).convert()
         }
 
         convertImagesToGrayMonochrome()
