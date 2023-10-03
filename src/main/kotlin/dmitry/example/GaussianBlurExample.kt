3afdd874c53a2512b1c0f9ac0b72580fce76b321
Dmitry <xtopz1337@gmail.com>
2023-10-03 23:04:27 +0300

    add: GaussianBlurExample

---
+++ src/main/kotlin/dmitry/example/GaussianBlurExample.kt
@@ -0,0 +1,17 @@
+package dmitry.example
+
+import dmitry.example.GaussianBlurExample.inImagePath
+import dmitry.example.GaussianBlurExample.outImagePath
+import dmitry.filter.matrix.GaussianBlur
+import dmitry.store.ImageLoader
+import dmitry.store.ImageSaver
+
+private object GaussianBlurExample {
+    val inImagePath = PathToExampleEvaluator.evaluateImagePath<GaussianBlurExample>("original")
+    val outImagePath = PathToExampleEvaluator.evaluateImagePath<GaussianBlurExample>("blurred")
+}
+
+fun main() {
+    val blurredImage = GaussianBlur(ImageLoader(inImagePath).load()).convert()
+    ImageSaver(blurredImage, outImagePath).save()
+}
