7976965a69f2bb3e8a91a8e37eb825e0448e4294
Dmitry <xtopz1337@gmail.com>
2023-10-03 20:56:03 +0300

    add: region range and some properties

--- src/main/kotlin/dmitry/ImageRegion.kt
+++ src/main/kotlin/dmitry/ImageRegion.kt
@@ -4,23 +4,34 @@ import java.awt.image.BufferedImage
 
 class ImageRegion(
     val regionStart: Point = Point(0, 0),
-    val regionEnd: Point) {
+    val regionEnd: Point,
+    sourceImage: BufferedImage
+) {
 
     val lineHeight get() = IntRange(regionStart.y, regionEnd.y)
     val lineWidth get() = IntRange(regionStart.x, regionEnd.x)
 
+    val width get() = lineWidth.last - lineWidth.first
+    val height get() = lineHeight.last - lineHeight.first
+
     init {
         if (regionStart.x > regionEnd.x || regionStart.y > regionEnd.y) {
-            throw IllegalArgumentException("regionEnd must be >= regionStart")
+            throw IllegalArgumentException("start point mustn't be higher then end point")
+        }
+
+        if (lineHeight.first < 0 || lineHeight.last >= sourceImage.height
+            || lineWidth.first < 0 || lineWidth.last >= sourceImage.width) {
+            throw IllegalArgumentException("region must be inside an image square")
         }
     }
 
-    constructor(image: BufferedImage): this(regionEnd = Point(image.width - 1, image.height - 1))
-
-
+    constructor(image: BufferedImage): this(
+        regionEnd = Point(image.width - 1, image.height - 1), sourceImage = image)
 
     fun square(): Int {
-        return (lineHeight.last - lineHeight.first) * (lineWidth.last - lineWidth.first)
+        return width * height
     }
+
+
 }
 
