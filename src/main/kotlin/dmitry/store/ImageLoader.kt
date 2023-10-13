4cbccedc1c6393da8449c0a4b759aea7032567c8
Dmitry <xtopz1337@gmail.com>
2023-10-13 10:34:11 +0300

    upd: change class to object represent and add other load method

--- src/main/kotlin/dmitry/store/ImageLoader.kt
+++ src/main/kotlin/dmitry/store/ImageLoader.kt
@@ -2,10 +2,13 @@ package dmitry.store
 
 import java.awt.image.BufferedImage
 import java.io.File
+import java.io.InputStream
 import javax.imageio.ImageIO
 
-class ImageLoader(private val path: String) {
+object ImageLoader {
 
-    fun load(): BufferedImage = ImageIO.read(File(path))
+    fun load(path: String): BufferedImage = ImageIO.read(File(path))
+    fun load(stream: InputStream): BufferedImage = ImageIO.read(stream)
+
 
 }
