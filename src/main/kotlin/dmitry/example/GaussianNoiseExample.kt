287b8b8a1a083668c5a34b94f7b340b4d2253ecf
Dmitry <xtopz1337@gmail.com>
2023-10-16 21:14:07 +0300

    change: sigma and m params

--- src/main/kotlin/dmitry/example/GaussianNoiseExample.kt
+++ src/main/kotlin/dmitry/example/GaussianNoiseExample.kt
@@ -5,6 +5,7 @@ import dmitry.example.GaussianNoiseExample.outImagePath
 import dmitry.noise.additive.GaussianNoiseGenerator
 import dmitry.store.ImageLoader
 import dmitry.store.ImageSaver
+import kotlin.math.*
 
 private object GaussianNoiseExample {
     val inImagePath = PathToExampleEvaluator.defaultInputPath<GaussianNoiseExample>()
@@ -13,7 +14,13 @@ private object GaussianNoiseExample {
 
 fun main() {
 
-    val generatedImage = GaussianNoiseGenerator(10.0, 20.0, .7, ImageLoader.load(inImagePath)).generate()
+    val generatedImage = GaussianNoiseGenerator(
+        1 /( 10 * sqrt(2 * PI) ),
+        128.0,
+        .25,
+        ImageLoader.load(inImagePath)
+    ).generate()
+
     ImageSaver(generatedImage, outImagePath).save()
 
 }
