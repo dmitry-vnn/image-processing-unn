928d575edfa8267fb4d94581b1776d4ec0864ac5
Dmitry <xtopz1337@gmail.com>
2023-10-23 20:26:09 +0300

    add: CannyOperator edge detection

---
+++ src/main/kotlin/dmitry/filter/edgedetection/CannyOperator.kt
@@ -0,0 +1,18 @@
+package dmitry.filter.edgedetection
+
+import dmitry.filter.ImageConverter
+import jcanny.JCanny
+import java.awt.image.BufferedImage
+
+class CannyOperator(
+    private val image: BufferedImage
+): ImageConverter{
+
+    private val cannyThresholdRatio = .2 //Suggested range .2 - .4
+    private val cannyStdDev = 1 //Range 1-3
+
+
+    override fun convert(): BufferedImage {
+        return JCanny.CannyEdges(image, cannyStdDev, cannyThresholdRatio)
+    }
+}
