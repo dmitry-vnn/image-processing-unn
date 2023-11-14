be19b3e83ff4a80731458ac78454d84332f03659
Dmitry <xtopz1337@gmail.com>
2023-11-15 00:59:19 +0300

    change: add equals and hashcode

--- src/main/kotlin/dmitry/imageprocessing/Point.kt
+++ src/main/kotlin/dmitry/imageprocessing/Point.kt
@@ -9,4 +9,19 @@ class Point(x: Int, y: Int) {
     operator fun component1() = x
 
     operator fun component2() = y
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Point) return false
+
+        if (pair != other.pair) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return pair.hashCode()
+    }
+
+
 }
