7e13a0bac7e14588e8e00b48392e330f4a3254ad
Dmitry <xtopz1337@gmail.com>
2023-11-15 22:09:30 +0300

    change: add image properties sample

--- src/main/kotlin/dmitry/example/ComponentsExample.kt
+++ src/main/kotlin/dmitry/example/ComponentsExample.kt
@@ -3,6 +3,9 @@ package dmitry.example
 import dmitry.example.ComponentsExample.inImagePath
 import dmitry.imageprocessing.shapeanalysis.marking.RecursiveBinaryImageMarker
 import dmitry.imageprocessing.shapeanalysis.marking.separator.MarkerSeparatorImpl
+import dmitry.imageprocessing.shapeanalysis.moments.CentralMomentCalculator
+import dmitry.imageprocessing.shapeanalysis.moments.ImagePropertiesCalculator
+import dmitry.imageprocessing.shapeanalysis.moments.RawMomentCalculator
 import dmitry.imageprocessing.store.ImageLoader
 
 private object ComponentsExample {
@@ -14,6 +17,22 @@ fun main() {
 
     val separatedComponentsMasks = MarkerSeparatorImpl(RecursiveBinaryImageMarker(originalImage)).separate()
 
+    separatedComponentsMasks.forEachIndexed {componentIndex, componentBinaryImage ->
+
+        val rawMomentCalculator = RawMomentCalculator(componentBinaryImage)
+        val propertiesCalculator = ImagePropertiesCalculator(
+            rawMomentCalculator,
+            CentralMomentCalculator(componentBinaryImage,
+                rawMomentCalculator)
+        )
+
+
+        println("shape #${componentIndex + 1}:")
+        println(" center = " + propertiesCalculator.centroid() )
+        println(" square = " + propertiesCalculator.square() )
+        println(" orientation angle = " + propertiesCalculator.orientationAngleDegree() )
+
+    }
     println("Shapes detected: ${separatedComponentsMasks.size}")
 
 }
