afb52eeaeaa4322edc5e1fdc9d6f03ae437a2216
Dmitry <xtopz1337@gmail.com>
2023-11-20 19:00:12 +0300

    add: boolean matrix

---
+++ src/main/kotlin/dmitry/imageprocessing/shapeanalysis/matrix/BoolMatrix.kt
@@ -0,0 +1,56 @@
+package dmitry.imageprocessing.shapeanalysis.matrix
+
+class BoolMatrix(
+    override val height: Int,
+    override val width: Int
+): MutableMatrix<Boolean> {
+
+    val matrix = Array(height) { BooleanArray(width) }
+
+    private val xRange = 0..<width
+    private val yRange = 0..<height
+
+    override fun set(y: Int, x: Int, value: Boolean) {
+        validatePoint(x, y)
+        matrix[y][x] = value
+    }
+
+    private fun validatePoint(x: Int, y: Int) {
+        if (x !in xRange) {
+            throw IndexOutOfBoundsException("x = $x out of $xRange")
+        }
+
+        if (y !in yRange) {
+            throw IndexOutOfBoundsException("y = $y out of $yRange")
+        }
+
+    }
+
+    override fun get(y: Int, x: Int): Boolean {
+        validatePoint(x, y)
+        return matrix[y][x]
+    }
+
+    override fun forEachIndexed(action: (x: Int, y: Int, element: Boolean) -> Unit) {
+        for (y in yRange) {
+            for (x in xRange) {
+                action(x, y, matrix[y][x])
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return StringBuilder().apply {
+            for (y in yRange) {
+                for (x in xRange) {
+                    append(matrix[y][x])
+                    if (x < xRange.last) {
+                        append(", ")
+                    }
+                }
+                append('\n')
+            }
+        }.toString()
+    }
+
+}
