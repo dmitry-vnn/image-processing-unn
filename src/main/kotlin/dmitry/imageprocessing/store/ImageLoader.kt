4c4f5191c538ee8bce93d2bad263fcce6164ee53
Dmitry <xtopz1337@gmail.com>
2023-11-20 18:53:32 +0300

    change: create folder if path not found

--- src/main/kotlin/dmitry/imageprocessing/store/ImageLoader.kt
+++ src/main/kotlin/dmitry/imageprocessing/store/ImageLoader.kt
@@ -2,12 +2,17 @@ package dmitry.imageprocessing.store
 
 import java.awt.image.BufferedImage
 import java.io.File
-import java.io.InputStream
 import javax.imageio.ImageIO
 
 object ImageLoader {
 
-    fun load(path: String): BufferedImage = ImageIO.read(File(path))
+    fun load(path: String): BufferedImage {
+        val file = File(path)
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+        return ImageIO.read(file)
+    }
 
 
 }
