40462197f41043de1d802667581e898d25966329
Dmitry <xtopz1337@gmail.com>
2023-11-28 21:27:42 +0300

    refactor: sample name changed to wavelet haar

--- src/main/kotlin/dmitry/example/WaveletHaarSample.kt
+++ src/main/kotlin/dmitry/example/WaveletHaarSample.kt
@@ -1,9 +1,9 @@
 package dmitry.example
 
-import dmitry.example.FrequenciesFilterSample.inImagePath
-import dmitry.example.FrequenciesFilterSample.lowerFrequenciesFilterImagePath
-import dmitry.example.FrequenciesFilterSample.reverseWaveletTransformImagePath
-import dmitry.example.FrequenciesFilterSample.upperFrequenciesFilterImagePath
+import dmitry.example.WaveletHaarSample.inImagePath
+import dmitry.example.WaveletHaarSample.lowerFrequenciesFilterImagePath
+import dmitry.example.WaveletHaarSample.reverseWaveletTransformImagePath
+import dmitry.example.WaveletHaarSample.upperFrequenciesFilterImagePath
 import dmitry.imageprocessing.compression.filters.LowerFrequenciesFilter
 import dmitry.imageprocessing.compression.filters.UpperFrequenciesFilter
 import dmitry.imageprocessing.compression.transform.haar.ForwardHaarTransformerImpl
@@ -11,12 +11,12 @@ import dmitry.imageprocessing.compression.transform.haar.ReverseHaarTransformerI
 import dmitry.imageprocessing.store.ImageLoader
 import dmitry.imageprocessing.store.ImageSaver
 
-private object FrequenciesFilterSample {
-    val inImagePath = PathToExampleEvaluator.evaluateImagePath<FrequenciesFilterSample>("in", "png")
+private object WaveletHaarSample {
+    val inImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("in", "png")
 
-    val upperFrequenciesFilterImagePath = PathToExampleEvaluator.evaluateImagePath<FrequenciesFilterSample>("upper-freq-filtered", "png")
-    val lowerFrequenciesFilterImagePath = PathToExampleEvaluator.evaluateImagePath<FrequenciesFilterSample>("lower-freq-filtered", "png")
-    val reverseWaveletTransformImagePath = PathToExampleEvaluator.evaluateImagePath<FrequenciesFilterSample>("reverse-wavelet", "png")
+    val upperFrequenciesFilterImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("upper-freq-filtered", "png")
+    val lowerFrequenciesFilterImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("lower-freq-filtered", "png")
+    val reverseWaveletTransformImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("reverse-wavelet", "png")
 }
 
 fun main() {
