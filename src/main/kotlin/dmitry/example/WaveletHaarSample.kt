749c65d2d8bfacef50ffdcc2d8dde9be3cc8ae1f
Dmitry <xtopz1337@gmail.com>
2023-11-29 18:44:04 +0300

    add: wavelet haar sample

--- src/main/kotlin/dmitry/example/WaveletHaarSample.kt
+++ src/main/kotlin/dmitry/example/WaveletHaarSample.kt
@@ -1,30 +1,48 @@
 package dmitry.example
 
+import dmitry.example.WaveletHaarSample.forwardOncePath
+import dmitry.example.WaveletHaarSample.forwardThricePath
+import dmitry.example.WaveletHaarSample.forwardTwicePath
 import dmitry.example.WaveletHaarSample.inImagePath
-import dmitry.example.WaveletHaarSample.lowerFrequenciesFilterImagePath
-import dmitry.example.WaveletHaarSample.reverseWaveletTransformImagePath
-import dmitry.example.WaveletHaarSample.upperFrequenciesFilterImagePath
-import dmitry.imageprocessing.compression.filters.LowerFrequenciesFilter
-import dmitry.imageprocessing.compression.filters.UpperFrequenciesFilter
-import dmitry.imageprocessing.compression.transform.haar.ForwardHaarTransformerImpl
-import dmitry.imageprocessing.compression.transform.haar.ReverseHaarTransformerImpl
+import dmitry.example.WaveletHaarSample.reverseOncePath
+import dmitry.example.WaveletHaarSample.reverseThricePath
+import dmitry.example.WaveletHaarSample.reverseTwicePath
 import dmitry.imageprocessing.store.ImageLoader
 import dmitry.imageprocessing.store.ImageSaver
+import dmitry.imageprocessing.wavelet.apply.ForwardHaarTransformRepeater
+import dmitry.imageprocessing.wavelet.apply.ReverseHaarTransformRepeater
 
 private object WaveletHaarSample {
     val inImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("in", "png")
 
-    val upperFrequenciesFilterImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("upper-freq-filtered", "png")
-    val lowerFrequenciesFilterImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("lower-freq-filtered", "png")
-    val reverseWaveletTransformImagePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("reverse-wavelet", "png")
+    val forwardOncePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("forward-once", "png")
+    val forwardTwicePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("forward-twice", "png")
+    val forwardThricePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("forward-thrice", "png")
+
+    val reverseOncePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("reverse-once", "png")
+    val reverseTwicePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("reverse-twice", "png")
+    val reverseThricePath = PathToExampleEvaluator.evaluateImagePath<WaveletHaarSample>("reverse-thrice", "png")
 }
 
 fun main() {
     val image = ImageLoader.load(inImagePath)
 
-    val frequencyContainer = ForwardHaarTransformerImpl(image).groupByFrequencies()
 
-    ImageSaver(LowerFrequenciesFilter(frequencyContainer).convert(), lowerFrequenciesFilterImagePath).save()
-    ImageSaver(UpperFrequenciesFilter(frequencyContainer).convert(), upperFrequenciesFilterImagePath).save()
-    ImageSaver(ReverseHaarTransformerImpl(frequencyContainer).createImageOnFrequencies(), reverseWaveletTransformImagePath).save()
+    val forwardRepeater = ForwardHaarTransformRepeater(image)
+
+    val forwardOnce = forwardRepeater.apply(1)
+    val forwardTwice = forwardRepeater.apply(2)
+    val forwardThrice = forwardRepeater.apply(3)
+
+    val reverseOnce = ReverseHaarTransformRepeater(forwardOnce, 1).apply()
+    val reverseTwice = ReverseHaarTransformRepeater(forwardTwice, 2).apply()
+    val reverseThrice = ReverseHaarTransformRepeater(forwardThrice, 3).apply()
+
+    ImageSaver(forwardOnce, forwardOncePath).save()
+    ImageSaver(forwardTwice, forwardTwicePath).save()
+    ImageSaver(forwardThrice, forwardThricePath).save()
+
+    ImageSaver(reverseOnce, reverseOncePath).save()
+    ImageSaver(reverseTwice, reverseTwicePath).save()
+    ImageSaver(reverseThrice, reverseThricePath).save()
 }
