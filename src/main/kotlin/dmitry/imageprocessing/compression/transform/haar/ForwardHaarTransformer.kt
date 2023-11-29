3d9dc5b0cd63946e67d3c06e80f69d7eecfa9492
Dmitry <xtopz1337@gmail.com>
2023-11-29 18:37:02 +0300

    remove: old implementations

--- src/main/kotlin/dmitry/imageprocessing/compression/transform/haar/ForwardHaarTransformer.kt
+++
@@ -1,9 +0,0 @@
-package dmitry.imageprocessing.compression.transform.haar
-
-import dmitry.imageprocessing.compression.FrequencyContainer
-
-interface ForwardHaarTransformer {
-
-    fun groupByFrequencies(): FrequencyContainer
-
-}
--- src/main/kotlin/dmitry/imageprocessing/compression/transform/haar/ReverseHaarTransformer.kt
+++
@@ -1,9 +0,0 @@
-package dmitry.imageprocessing.compression.transform.haar
-
-import java.awt.image.BufferedImage
-
-interface ReverseHaarTransformer {
-
-    fun createImageOnFrequencies(): BufferedImage
-
-}
